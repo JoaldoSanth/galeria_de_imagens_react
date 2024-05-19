393cf002373224c78ef29439d6fd581bcf8494dc
JoaldoSanth <joaldosant3@gmail.com>
2024-05-19 02:46:26 -0300

    projetos no react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Galeria_de_imagens from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Galeria_de_imagens />
+  </React.StrictMode>,
+)
